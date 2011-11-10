52c4a06c07da841349b00225c376bb68065f3d5a
Kostas Michalopoulos <badsector@runtimelegend.com>
2011-11-10 18:05:32 +0200

    Calling lil_parse with funclevel now resets the breakrun flag

--- lil.c
+++ lil.c
@@ -621,6 +621,7 @@ lil_value_t lil_parse(lil_t lil, const char* code, size_t codelen, int funclevel
     skip_spaces(lil);
     lil->parse_depth++;
     if (lil->parse_depth == 1) lil->error = 0;
+    if (funclevel) lil->env->breakrun = 0;
     while (lil->head < lil->clen && !lil->error) {
         if (words) lil_free_list(words);
         if (val) lil_free_value(val);
